982101bf3b92dcba0d49dda5e974259d7e0b5468
Danchou <dvoid367@gmail.com>
2024-06-18 15:20:10 +0800

    From packaging to user retrieval3

--- routes/web.php
+++ routes/web.php
@@ -73,10 +73,9 @@ Route::resource('reviews', ReviewController::class);
 Route::get('/shop', [shopController::class, 'index'])->name('shop.index');
 Route::get('/shop/products', [shopController::class, 'shop'])->name('shop.products');
 Route::get('/shop/products/details/{id}', [shopController::class, 'details'])->name('shop.details');
+Route::get('/shop/products/checkout', [shopController::class, 'checkout'])->name('shop.checkout');
 
 Route::get('/selectedItems/forPackaging', [SelectedItemsController::class, 'forPackaging'])->name('selectedItems.forPackaging');
 Route::get('/selectedItems/forDelivery', [SelectedItemsController::class, 'forDelivery'])->name('selectedItems.forDelivery');
 Route::get('/selectedItems/forPickup', [SelectedItemsController::class, 'forPickup'])->name('selectedItems.forPickup');
 Route::post('/selected-items/{referenceNo}/update', [SelectedItemsController::class, 'update'])->name('selected-items.update');
-
-Route::get('/shop/products/checkout', [shopController::class, 'checkout'])->name('shop.checkout');
