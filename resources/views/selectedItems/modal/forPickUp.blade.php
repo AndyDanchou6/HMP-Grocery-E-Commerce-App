d4c363205f105ddc78fc2a90a104e8e9c4f88532
AndyDanchou6 <132078888+AndyDanchou6@users.noreply.github.com>
2024-07-09 11:36:23 +0800

    Update forPickUp.blade.php

--- resources/views/selectedItems/modal/forPickUp.blade.php
+++ resources/views/selectedItems/modal/forPickUp.blade.php
@@ -1,4 +1,4 @@
-<div class="modal fade" id="forPickup{{$user['referenceNo']}}" tabindex="-1" aria-hidden="true">
+<div class="modal fade" id="forPickUp{{$user['referenceNo']}}" tabindex="-1" aria-hidden="true">
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
