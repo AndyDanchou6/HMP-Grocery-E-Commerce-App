4edc9b7f09b7b6466ffb0a62a77a920a3a29e267
AndyDanchou6 <132078888+AndyDanchou6@users.noreply.github.com>
2024-07-09 10:13:31 +0800

    Update forDelivery.blade.php

--- resources/views/selectedItems/modal/forDelivery.blade.php
+++ resources/views/selectedItems/modal/forDelivery.blade.php
@@ -1,4 +1,4 @@
-<div class="modal fade" id="messages{{$user['referenceNo']}}" tabindex="-1" aria-hidden="true">
+<div class="modal fade" id="forDelivery{{$user['referenceNo']}}" tabindex="-1" aria-hidden="true">
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
