581f5e0264c50d8d6f2ae35af42a1d67b63616bd
Jerry Molar Parrocha <133473676+Jparrocha2002@users.noreply.github.com>
2024-07-11 17:16:39 +0800

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
@@ -14,31 +13,17 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-
         $admin1 = 'user_avatar/avatar1.jpg';
         $admin2 = 'user_avatar/avatar2.jpg';
         $users = 'user_avatar/avatar.jpg';
 
-
-        User::factory()->create([
-            'role' => 'Admin',
-            'avatar' => $admin1,
-            'name' => 'Testing',
-            'email' => 'testing@admin.me',
-            'phone' => 123123123,
-            'fb_link' => 'https://www.facebook.com/',
-            'address' => 'Rizal Street, Sogod, Southern, Leyte',
-            'password' => Hash::make('12345678'),
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
-
-         User::factory()->create([
+        // Admin users
+        User::create([
             'role' => 'Admin',
             'avatar' => $admin1,
             'name' => 'E-Mart',
-            'email' => 'j.parrocha@gmail.com',
-            'phone' => 09638753244,
+            'email' => 'e.mart@admin.me',
+            'phone' => 639638753244,
             'fb_link' => 'https://www.facebook.com/',
             'address' => 'Rizal Street, Sogod, Southern, Leyte',
             'password' => Hash::make('12345678'),
@@ -46,53 +31,55 @@ class UserSeeder extends Seeder
             'updated_at' => now(),
         ]);
 
-        User::factory()->create([
+        User::create([
             'role' => 'Admin',
             'avatar' => $admin2,
             'name' => 'Melchard Lina',
             'email' => 'e.mart@eridiano.admin',
             'phone' => 639232423,
             'fb_link' => 'https://www.facebook.com/',
-            'address' => 'Kagwapo Street, kapogian, Southern Gwapo',
+            'address' => 'Kagwapo Street, Kapogian, Southern Gwapo',
             'password' => Hash::make('12345678'),
             'created_at' => now(),
             'updated_at' => now(),
         ]);
 
-        User::factory()->create([
+        // Customer user
+        User::create([
             'role' => 'Customer',
             'avatar' => $users,
             'name' => 'Elessa Fermano',
             'email' => 'esang@ungo.com',
             'phone' => 639232423,
             'fb_link' => 'https://www.facebook.com/',
-            'address' => 'Kagwapo Street, kagwapahan, Southern Gwapa',
+            'address' => 'Kagwapo Street, Kagwapahan, Southern Gwapa',
             'password' => Hash::make('12345678'),
             'created_at' => now(),
             'updated_at' => now(),
         ]);
 
-        User::factory()->create([
+        // Courier users
+        User::create([
             'role' => 'Courier',
             'avatar' => $users,
             'name' => 'Antonette Lozares',
             'email' => 'tonette@ungo.com',
             'phone' => 639232423,
             'fb_link' => 'https://www.facebook.com/',
-            'address' => 'Kagwapo Street, kagwapahan, Southern Gwapa',
+            'address' => 'Kagwapo Street, Kagwapahan, Southern Gwapa',
             'password' => Hash::make('12345678'),
             'created_at' => now(),
             'updated_at' => now(),
         ]);
 
-        User::factory()->create([
+        User::create([
             'role' => 'Courier',
             'avatar' => $users,
             'name' => 'Janine Rosales',
             'email' => 'anine@ungo.com',
             'phone' => 639232423,
             'fb_link' => 'https://www.facebook.com/',
-            'address' => 'Kagwapo Street, kagwapahan, Southern Gwapa',
+            'address' => 'Kagwapo Street, Kagwapahan, Southern Gwapa',
             'password' => Hash::make('12345678'),
             'created_at' => now(),
             'updated_at' => now(),
