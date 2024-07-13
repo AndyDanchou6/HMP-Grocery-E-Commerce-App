dfb81129500a6970ff904d5c927b00376a98936f
AndyDanchou6 <132078888+AndyDanchou6@users.noreply.github.com>
2024-07-13 09:23:38 +0800

    Update deliveryInfo.blade.php

--- resources/views/selectedItems/modal/deliveryInfo.blade.php
+++ resources/views/selectedItems/modal/deliveryInfo.blade.php
@@ -38,7 +38,7 @@
 
                         <div class="col-6 col-sm-3 col-md-2 mb-3">
                             <label for="quantity" class="col-6 col-sm-3 col-md-2 col-form-label">Quantity</label>
-                            <input type="number" class="form-control item-quantity" data-item-id="{{ $user['referenceNo'].'_'.$item->id }}" value="{{ $user['quantity'] }}" readonly>
+                            <input type="number" class="form-control item-quantity" data-item-id="{{ $user['referenceNo'].'_'.$item->id }}" value="{{ $item->quantity }}" readonly>
                         </div>
 
                         <div class="col-6 col-sm-3 col-md-3 mb-3">
