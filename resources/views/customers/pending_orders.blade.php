093c360d90d172c83bd1c9fab1185c6c1c892723
Jerry Molar Parrocha <133473676+Jparrocha2002@users.noreply.github.com>
2024-07-18 08:10:09 +0800

    Update pending_orders.blade.php

--- resources/views/customers/pending_orders.blade.php
+++ resources/views/customers/pending_orders.blade.php
@@ -1,29 +1,6 @@
 @extends('app')
 
 @section('content')
-<div class="container-xxl flex-grow-1 container-p-y">
-    <div class="card">
-        <div class="card-header row justify-content-between align-items-center">
-            <h4 class="col-md-3"><span class="badge bg-label-danger me-1">Pending Orders</span></h4>
-        </div>
-        <div class="table-responsive text-nowrap">
-            <table class="table table-hover">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Reference No.</th>
-                        <th>Order Type</th>
-                        <th>Payment Type</th>
-                        <th>Payment Condition</th>
-                        <th>Items</th>
-                    </tr>
-                </thead>
-                <tbody class="table-border-bottom-0" id="tableBody">
 
-                </tbody>
-            </table>
-        </div>
-    </div>
-</div>
-</div>
+
 @endsection
